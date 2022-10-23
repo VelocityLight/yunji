7a1e00b13ce279949d81f4f96af17d2ea9d187f1
李煜超 <yuchao.li@pingcap.com>
2022-10-23 12:46:15 +0800

    rush rush

--- website/src/pages/BillingTrend.js
+++ website/src/pages/BillingTrend.js
@@ -1,14 +1,21 @@
 import * as React from "react";
 import { useEffect, useState } from 'react';
 import { useQuery, useQueryClient } from "react-query";
+import moment from 'moment';
 import MyLayout from "../layout/Layout";
 import PileBarChart from "../graphs/PileBarChart";
 import { fetchTags, fetchServices } from "../api/filter_api"
 import MultiSelector from "../components/MultiSelector"
+
+import { Select } from 'antd';
 import Selector from "../components/SingleSelector"
-import { Divider, Tag } from 'antd';
+import { Divider, Tag, DatePicker } from 'antd';
 
 import { fetchBillingByTagAndService } from "../api/billing_api"
+
+
+const { Option } = Select;
+const { RangePicker } = DatePicker;
 
 export function getSelectorValues(tagResp) {
   return tagResp.map(tag => tag.name)
@@ -45,7 +52,6 @@ const BillingTrendPage = () => {
 
   )
 
-
   useEffect(() => {
     fetchBillingByTagAndService({ tags: selectedTags, service: selectedService })
       .then((resp) => setInputData(resp.body == undefined ? [] : resp.body));
@@ -63,19 +69,50 @@ const BillingTrendPage = () => {
   const serviceSelectors = getSelectorValues(serviceQuery.data)
   // setInputData(query.data.body == undefined ? [] : query.data.body)
   // // const inputData = query.data.body == undefined ? [] : query.data.body
-
+  const dateFormat = 'YYYYMMDD';
   return (
     <MyLayout>
       <div>
-        <Tag color="cyan">Tags</Tag>
+        <Tag style={{
+          margin: '5px',
+        }}
+          color="cyan">Time</Tag>
+        <RangePicker
+          defaultValue={[moment('20220801', dateFormat), moment('20220831', dateFormat)]}
+          disabled={[false, false]}
+        />
+        <Tag style={{
+          margin: '5px',
+        }}
+          color="cyan">Tags</Tag>
         <MultiSelector key="tags" items={tagSelectors} onSelect={onSelectTags} />
-        <Tag color="cyan">Service</Tag>
+        <Tag style={{
+          margin: '5px',
+        }}
+          color="cyan">Service</Tag>
         <MultiSelector key="services" items={serviceSelectors} onSelect={onSelectService} />
+
+
+        <Tag style={{
+          margin: '5px',
+        }}
+          color="cyan">Cloud</Tag>
+        <Select
+          defaultValue="AWS"
+          style={{
+            width: "10%",
+          }}
+        >
+          <Option key="AWS">AWS</Option>
+          <Option key="GCP" disabled>GCP</Option>
+          <Option key="AZURE" disabled>AZURE</Option>
+          <Option key="Ali Cloud" disabled>Ali Cloud</Option>
+        </Select>
       </div>
       <div>
         <PileBarChart data={inputData} />
       </div>
-    </MyLayout>
+    </MyLayout >
   );
 };
 
